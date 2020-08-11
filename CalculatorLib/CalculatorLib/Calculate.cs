3f88d15d928c0ee1ef72d2e806ad2dd86534996e
Rajani Joshi <68274751+Rajani03Zensar@users.noreply.github.com>
2020-08-11 19:20:47 +0530

    Add Class Library

---
+++ CalculatorLib/CalculatorLib/Calculate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+
+
+namespace CalculatorLib
+{
+    [Serializable]
+    public class Calculate
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public int Addition()
+        {
+            return num1 + num2;
+        }
+        public int Subtract()
+        {
+            return num1 - num2;
+        }
+        public int Multiply()
+        {
+            return num1 * num2;
+        }
+        public double Divide()
+        {
+            return num1 / num2;
+        }
+       
+    }
+    
+}
