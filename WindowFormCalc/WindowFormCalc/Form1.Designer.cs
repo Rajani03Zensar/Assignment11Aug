80cc64754b40b580eaeb910f0eac0b2a2822e092
Rajani Joshi <68274751+Rajani03Zensar@users.noreply.github.com>
2020-08-11 19:36:06 +0530

    Add WindowFormCalc file

---
+++ WindowFormCalc/WindowFormCalc/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace WindowFormCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnMulti = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.lblAllMethod = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Num1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Num2";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(205, 47);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(197, 26);
+            this.txtNum1.TabIndex = 2;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(205, 132);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(197, 26);
+            this.txtNum2.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(115, 208);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(131, 36);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Addition";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Location = new System.Drawing.Point(326, 208);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(147, 36);
+            this.btnSubtract.TabIndex = 5;
+            this.btnSubtract.Text = "Subtract";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnMulti
+            // 
+            this.btnMulti.Location = new System.Drawing.Point(543, 208);
+            this.btnMulti.Name = "btnMulti";
+            this.btnMulti.Size = new System.Drawing.Size(144, 36);
+            this.btnMulti.TabIndex = 6;
+            this.btnMulti.Text = "Multiplication ";
+            this.btnMulti.UseVisualStyleBackColor = true;
+            this.btnMulti.Click += new System.EventHandler(this.btnMulti_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(326, 312);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(154, 40);
+            this.btnDivide.TabIndex = 7;
+            this.btnDivide.Text = "Division";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // lblAllMethod
+            // 
+            this.lblAllMethod.AutoSize = true;
+            this.lblAllMethod.Location = new System.Drawing.Point(86, 363);
+            this.lblAllMethod.Name = "lblAllMethod";
+            this.lblAllMethod.Size = new System.Drawing.Size(0, 20);
+            this.lblAllMethod.TabIndex = 8;
+            this.lblAllMethod.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAllMethod);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMulti);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnMulti;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Label lblAllMethod;
+    }
+}
+
---
+++ WindowFormCalc/WindowFormCalc/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CalculatorLib;
+
+namespace WindowFormCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Calculate cal;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            cal = new Calculate();
+            cal.num1 = Convert.ToInt32(txtNum1.Text);
+            cal.num2 = Convert.ToInt32(txtNum2.Text);
+            lblAllMethod.Text = "Addition is " + cal.Addition().ToString();
+            txtNum1.Text = "";
+            txtNum2.Text = "";
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            cal = new Calculate();
+            cal.num1 = Convert.ToInt32(txtNum1.Text);
+            cal.num2 = Convert.ToInt32(txtNum2.Text);
+            lblAllMethod.Text = "Subtraction is " + cal.Subtract().ToString();
+            txtNum1.Text = "";
+            txtNum2.Text = "";
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+            cal = new Calculate();
+            cal.num1 = Convert.ToInt32(txtNum1.Text);
+            cal.num2 = Convert.ToInt32(txtNum2.Text);
+            lblAllMethod.Text = "Multiplication is " + cal.Multiply().ToString();
+            txtNum1.Text = "";
+            txtNum2.Text = "";
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            cal = new Calculate();
+            cal.num1 = Convert.ToInt32(txtNum1.Text);
+            cal.num2 = Convert.ToInt32(txtNum2.Text);
+            lblAllMethod.Text = "Quotient is " + cal.Divide().ToString();
+            txtNum1.Text = "";
+            txtNum2.Text = "";
+        }
+       
+    }
+}
